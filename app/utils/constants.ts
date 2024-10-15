7042e2e6d2bd68675d741ce0b935ab3b87404fa7
bartwisch <123270793+bartwisch@users.noreply.github.com>
2024-10-15 21:23:04 +0200

    llama3.2 local and default

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -2,7 +2,7 @@ export const WORK_DIR_NAME = 'project';
 export const WORK_DIR = `/home/${WORK_DIR_NAME}`;
 export const MODIFICATIONS_TAG_NAME = 'bolt_file_modifications';
 export const MODEL_REGEX = /^\[Model: (.*?)\]\n\n/;
-export const DEFAULT_MODEL = "claude-3-5-sonnet-20240620";
+export const DEFAULT_MODEL = "llama3.2" //"claude-3-5-sonnet-20240620";
 export const DEFAULT_PROVIDER = "Anthropic";
 export const MODEL_LIST = [
     { name: 'claude-3-5-sonnet-20240620', label: 'Claude 3.5 Sonnet', provider: 'Anthropic' },
@@ -17,6 +17,7 @@ export const MODEL_LIST = [
     { name: 'codellama:34b', label: 'Code Llama 34b', provider: 'Ollama' },
     { name: 'codellama:13b', label: 'Code Llama 13b', provider: 'Ollama' },
     { name: 'codellama:7b', label: 'Code Llama 7b', provider: 'Ollama' },
+    { name: 'llama3.2', label: 'llama3.2 (local model)', provider: 'Ollama' },
     { name: 'deepseek/deepseek-coder', label: 'Deepseek-Coder V2 236B (OpenRouter)', provider: 'OpenRouter' },
     { name: 'google/gemini-flash-1.5', label: 'Google Gemini Flash 1.5 (OpenRouter)', provider: 'OpenRouter' },
     { name: 'google/gemini-pro-1.5', label: 'Google Gemini Pro 1.5 (OpenRouter)', provider: 'OpenRouter' },
